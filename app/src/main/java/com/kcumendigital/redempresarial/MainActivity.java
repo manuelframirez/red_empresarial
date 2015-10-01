8844b35dd74400ae68583277ebe918c6999cf7d5
manuelframirez <manuel.ramirez.v@gmail.com>
2015-10-01 08:33:29 -0500

    changes details buxtar img

--- app/src/main/java/com/kcumendigital/redempresarial/MainActivity.java
+++ app/src/main/java/com/kcumendigital/redempresarial/MainActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
